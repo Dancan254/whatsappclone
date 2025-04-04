c7a20ea25f1e1a0aa50f4d21d72bbbee57be5573
Dancan254 <dancanian25@gmail.com>
2025-04-04 11:57:42 +0300

    Add ChatResponse class for chat response data transfer

---
+++ src/main/java/com/mongs/whatsappclone/chat/ChatResponse.java
@@ -0,0 +1,22 @@
+package com.mongs.whatsappclone.chat;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatResponse {
+
+    private String id;
+    private String name;
+    private long unreadCount;
+    private String lastMessage;
+    private LocalDateTime lastMessageTime;
+    private boolean isRecipientOnline;
+    private String senderId;
+    private String recipientId;
+}
