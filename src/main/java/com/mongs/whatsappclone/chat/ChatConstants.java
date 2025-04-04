790f7d08240f02c4f92a8c8d5a9b60f6aa890e4e
Dancan254 <dancanian25@gmail.com>
2025-04-04 11:56:59 +0300

    Add ChatConstants class for chat query constants

---
+++ src/main/java/com/mongs/whatsappclone/chat/ChatConstants.java
@@ -0,0 +1,6 @@
+package com.mongs.whatsappclone.chat;
+
+public class ChatConstants {
+    public static final String FIND_CHAT_BY_SENDER_ID = "Chat.findChatBySenderId";
+    public static final String FIND_CHAT_BY_SENDER_ID_AND_RECIPIENT_ID = "Chat.findChatBySenderIdAndRecipientId";
+}
