0bc80cbf681335f28b4bb1d46fa949920ff65e4c
Dancan254 <dancanian25@gmail.com>
2025-04-04 18:28:35 +0300

    Add OpenAPI tag to UserController and configure default media type in application.yml

--- src/main/java/com/mongs/whatsappclone/user/UserController.java
+++ src/main/java/com/mongs/whatsappclone/user/UserController.java
@@ -1,5 +1,6 @@
 package com.mongs.whatsappclone.user;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "User API")
 public class UserController {
 
     private final UserService userService;
