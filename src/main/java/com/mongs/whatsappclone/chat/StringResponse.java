196fc0b801d66a0bedb9305dc2dd5ac4d776764f
Dancan254 <dancanian25@gmail.com>
2025-04-04 11:58:02 +0300

    Add StringResponse class for handling string response data transfer

---
+++ src/main/java/com/mongs/whatsappclone/chat/StringResponse.java
@@ -0,0 +1,12 @@
+package com.mongs.whatsappclone.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StringResponse {
+    String response;
+}
