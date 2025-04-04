2c2d16de826fd38ee4290f4378f1da9098601fb4
Dancan254 <dancanian25@gmail.com>
2025-04-04 17:29:43 +0300

    Add recipientId field to Notification class for enhanced notification targeting

--- src/main/java/com/mongs/whatsappclone/notification/Notification.java
+++ src/main/java/com/mongs/whatsappclone/notification/Notification.java
@@ -13,6 +13,7 @@ public class Notification {
     private String chatId;
     private String content;
     private String senderId;
+    private String recipientId;
     private String chatName;
     private MessageType messageType;
     private NotificationType notificationType;
