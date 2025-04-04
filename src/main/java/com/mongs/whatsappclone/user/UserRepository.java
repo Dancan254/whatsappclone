63478975b8139bb9d5d259ce869d1a89cbc3abee
Dancan254 <dancanian25@gmail.com>
2025-04-04 13:57:02 +0300

    Add method to find all users except the authenticated user in UserRepository

--- src/main/java/com/mongs/whatsappclone/user/UserRepository.java
+++ src/main/java/com/mongs/whatsappclone/user/UserRepository.java
@@ -2,7 +2,10 @@ package com.mongs.whatsappclone.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.Authentication;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
     Optional<User> findByUserId(String publicId);
+
+    @Query(name = UserConstants.FIND_ALL_USERS_EXCEPT_SELF)
+    List<User> findAllUsersExceptSelf(Authentication authentication);
 }
