aad1592f95a9bd41a6f8a5953ce16913b001d470
Dancan254 <dancanian25@gmail.com>
2025-04-04 11:58:07 +0300

    Add FileService for handling file uploads

---
+++ src/main/java/com/mongs/whatsappclone/file/FileService.java
@@ -0,0 +1,11 @@
+package com.mongs.whatsappclone.file;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+    public String saveFile(MultipartFile file) {
+        return null;
+    }
+}
