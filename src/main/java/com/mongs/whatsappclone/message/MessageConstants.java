2af3dadadc6bd01132847c4875f4f1548d55f88f
Dancan254 <dancanian25@gmail.com>
2025-04-04 11:59:29 +0300

    Add MessageConstants for defining message-related query constants

---
+++ src/main/java/com/mongs/whatsappclone/message/MessageConstants.java
@@ -0,0 +1,7 @@
+package com.mongs.whatsappclone.message;
+
+public class MessageConstants {
+
+    public static final String FIND_MESSAGES_BY_CHAT_ID = "Messages.findMessagesByChatId";
+    public static final String SET_MESSAGES_TO_BE_SEEN_BY_CHAT = "Messages.setMessagesToBeSeenByChat";
+}
