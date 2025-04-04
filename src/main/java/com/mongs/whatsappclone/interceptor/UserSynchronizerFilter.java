7105a4e8d60d16400bef3a3b833d5ce3b097bc62
Dancan254 <dancanian25@gmail.com>
2025-04-04 18:28:21 +0300

    Add missing filter chain continuation in UserSynchronizerFilter

--- src/main/java/com/mongs/whatsappclone/interceptor/UserSynchronizerFilter.java
+++ src/main/java/com/mongs/whatsappclone/interceptor/UserSynchronizerFilter.java
@@ -33,5 +33,6 @@ public class UserSynchronizerFilter extends OncePerRequestFilter {
             JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
             userSynchronizer.synchronizeWithIdp(token.getToken());
         }
+        filterChain.doFilter(request, response);
     }
 }
